e680852f6d330e32ad49115e880ff3d7001f3ed6
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2012-04-28 13:48:59 +0000

    Tagging 2.23.1
    git-svn-id: http://svn.code.sf.net/p/angelscript/code@1289 404ce1b2-830e-0410-a2e2-b09542c77caf

---
+++ tags/2.23.1/sdk/tests/test_feature/source/test_namespace.cpp
@@ -0,0 +1,175 @@
+#include "utils.h"
+
+namespace TestNamespace
+{
+
+bool Test()
+{
+	bool fail = false;
+	asIScriptEngine *engine;
+	int r;
+	COutStream out;
+
+	{
+		engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+		engine->SetMessageCallback(asMETHOD(COutStream, Callback), &out, asCALL_THISCALL);
+		engine->RegisterGlobalFunction("void assert(bool)", asFUNCTION(Assert), asCALL_GENERIC);
+
+		const char *script =
+			"int func() { return var; } \n"
+			"int func2() { return var; } \n"
+			"int var = 0; \n"
+			"class cl { cl() {v = 0;} int v; } \n"
+			"interface i {} \n"
+			"enum e { e1 = 0 } \n"
+			"funcdef void fd(); \n"
+			// Namespaces allow same entities to be declared again
+			"namespace a { \n"
+			"  int func() { return var; } \n" // Should find the global var in the same namespace
+			"  int func2() { return func(); } \n" // Should find the global function in the same namespace
+			"  int var = 1; \n"
+			"  class cl { cl() {v = 1;} int v; } \n"
+			"  interface i {} \n"
+			"  enum e { e1 = 1 } \n"
+			"  funcdef void fd(); \n"
+			// Nested namespaces are allowed
+			"  namespace b { \n"
+			"    int var = 2; \n"
+			"  } \n"
+			"} \n"
+			// It's possible to specify exactly which one is wanted
+			"cl gc; \n"
+			"a::cl gca; \n"
+			"void main() \n"
+			"{ \n"
+			"  assert(var == 0); \n"
+			"  assert(::var == 0); \n"
+			"  assert(a::var == 1); \n"
+			"  assert(a::b::var == 2); \n"
+			"  assert(func() == 0); \n"
+			"  assert(a::func() == 1); \n"
+			"  assert(func2() == 0); \n"
+			"  assert(a::func2() == 1); \n"
+			"  assert(e1 == 0); \n"
+			"  assert(::e1 == 0); \n"
+			"  assert(e::e1 == 0); \n"
+			"  assert(::e::e1 == 0); \n"
+			"  assert(a::e1 == 1); \n"
+			"  assert(a::e::e1 == 1); \n"
+			"  cl c; \n"
+			"  a::cl ca; \n"
+			"  assert(c.v == 0); \n"
+			"  assert(ca.v == 1); \n"
+			"  assert(gc.v == 0); \n"
+			"  assert(gca.v == 1); \n"
+			"} \n";
+
+		asIScriptModule *mod = engine->GetModule("mod", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("", script);
+		r = mod->Build();
+		if( r < 0 )
+			TEST_FAILED;
+
+		r = ExecuteString(engine, "main()", mod);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+	
+		// Retrieving entities should work properly with namespace
+		mod->SetDefaultNamespace("a");
+		asIScriptFunction *f1 = mod->GetFunctionByDecl("int func()");
+		asIScriptFunction *f2 = mod->GetFunctionByDecl("int a::func()");
+		asIScriptFunction *f3 = mod->GetFunctionByName("func");
+		if( f1 == 0 || f1 != f2 || f1 != f3 )
+			TEST_FAILED;
+
+		int v1 = mod->GetGlobalVarIndexByName("var");
+		int v2 = mod->GetGlobalVarIndexByDecl("int var");
+		int v3 = mod->GetGlobalVarIndexByDecl("int a::var");
+		if( v1 < 0 || v1 != v2 || v1 != v3 )
+			TEST_FAILED;
+
+		int t1 = mod->GetTypeIdByDecl("cl");
+		int t2 = mod->GetTypeIdByDecl("a::cl");
+		if( t1 < 0 || t1 != t2 )
+			TEST_FAILED;
+
+		// Test saving and loading 
+		CBytecodeStream s("");
+		mod->SaveByteCode(&s);
+
+		asIScriptModule *mod2 = engine->GetModule("mod2", asGM_ALWAYS_CREATE);
+		r = mod2->LoadByteCode(&s);
+		if( r < 0 )
+			TEST_FAILED;
+
+		r = ExecuteString(engine, "main()", mod2);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		engine->Release();
+	}
+
+	// TODO: It should be possible to inform the namespace when querying by declaration
+	// TODO: It should be possible to choose whether to include namespace or not when getting declarations
+
+	// Test registering interface with namespace
+	{
+		engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+		engine->SetMessageCallback(asMETHOD(COutStream, Callback), &out, asCALL_THISCALL);
+		
+		r = engine->SetDefaultNamespace("test"); assert( r >= 0 );
+
+		r = engine->RegisterObjectType("t", 0, asOBJ_REF); assert( r >= 0 );
+		r = engine->RegisterObjectBehaviour("t", asBEHAVE_ADDREF, "void f()", asFUNCTION(0), asCALL_GENERIC); assert( r >= 0 );
+		r = engine->RegisterObjectMethod("t", "void f()", asFUNCTION(0), asCALL_GENERIC); assert( r >= 0 );
+		r = engine->RegisterObjectProperty("t", "int a", 0); assert( r >= 0 );
+		int t1 = engine->GetTypeIdByDecl("t");
+		int t2 = engine->GetTypeIdByDecl("test::t");
+		if( t1 < 0 || t1 != t2 )
+			TEST_FAILED;
+		
+		r = engine->RegisterInterface("i"); assert( r >= 0 );
+		r = engine->RegisterInterfaceMethod("i", "void f()"); assert( r >= 0 );
+		t1 = engine->GetTypeIdByDecl("test::i");
+		if( t1 < 0 )
+			TEST_FAILED;
+
+		r = engine->RegisterEnum("e"); assert( r >= 0 );
+		r = engine->RegisterEnumValue("e", "e1", 0); assert( r >= 0 );
+		t1 = engine->GetTypeIdByDecl("test::e");
+		if( t1 < 0 )
+			TEST_FAILED;
+
+		r = engine->RegisterFuncdef("void f()"); assert( r >= 0 );
+		t1 = engine->GetTypeIdByDecl("test::f");
+		if( t1 < 0 )
+			TEST_FAILED;
+
+		r = engine->RegisterGlobalFunction("void gf()", asFUNCTION(0), asCALL_GENERIC); assert( r >= 0 );
+		asIScriptFunction *f1 = engine->GetGlobalFunctionByDecl("void test::gf()");
+		asIScriptFunction *f2 = engine->GetGlobalFunctionByDecl("void gf()");
+		if( f1 == 0 || f1 != f2 )
+			TEST_FAILED;
+
+		r = engine->RegisterGlobalProperty("int gp", (void*)1); assert( r >= 0 );
+		int g1 = engine->GetGlobalPropertyIndexByName("gp");
+		int g2 = engine->GetGlobalPropertyIndexByDecl("int gp");
+		int g3 = engine->GetGlobalPropertyIndexByDecl("int test::gp");
+		if( g1 < 0 || g1 != g2 || g1 != g3 )
+			TEST_FAILED;
+
+		r = engine->RegisterTypedef("td", "int"); assert( r >= 0 );
+		t1 = engine->GetTypeIdByDecl("test::td");
+		if( t1 < 0 )
+			TEST_FAILED;
+
+		engine->Release();
+	}
+
+
+	// Success
+	return fail;
+}
+
+} // namespace
+
---
+++ tags/2.23.1/sdk/tests/test_feature/source/test_shared.cpp
@@ -0,0 +1,341 @@
+#include "utils.h"
+
+namespace TestShared
+{
+
+bool Test()
+{
+	bool fail = false;
+	CBufferedOutStream bout;
+	asIScriptEngine *engine;
+	int r;
+
+	{
+		int reg;
+ 		engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+		engine->SetMessageCallback(asMETHOD(CBufferedOutStream,Callback), &bout, asCALL_THISCALL);
+		engine->RegisterGlobalFunction("void assert(bool)", asFUNCTION(Assert), asCALL_GENERIC);
+		engine->RegisterGlobalProperty("int reg", &reg);
+
+		RegisterScriptArray(engine, true);
+
+		engine->RegisterEnum("ESHARED");
+		engine->RegisterEnumValue("ESHARED", "ES1", 1);
+
+		asIScriptModule *mod = engine->GetModule("", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("a", 
+			"interface badIntf {} \n"
+			"shared interface sintf {} \n"
+			"shared class T : sintf, badIntf \n" // Allow shared interface, but not non-shared interface
+			"{ \n"
+			"  void test() \n"
+			"  { \n"
+			"    var = 0; \n" // Don't allow accessing non-shared global variables
+			"    gfunc(); \n" // Don't allow calling non-shared global functions
+			"    reg = 1; \n" // Allow accessing registered variables
+			"    assert( reg == 1 ); \n" // Allow calling registered functions
+			"    badIntf @intf; \n" // Do not allow use of non-shared types in parameters/return type
+			"    int cnst = g_cnst; \n" // Allow using literal consts, even if they are declared in global scope
+			"    ESHARED es = ES1; \n" // Allow
+			"    ENOTSHARED ens = ENS1; \n" // Do not allow. The actual value ENS1 is allowed though, as it is a literal constant
+			"    cast<badIntf>(null); \n" // do not allow casting to non-shared types
+			"    assert !is null; \n" // Allow taking address of registered functions
+			"    gfunc !is null; \n" // Do not allow taking address of non-shared script functions
+			"    nonShared(); \n" // Do not allow constructing objects of non-shared type
+			"    impfunc(); \n" // Do not allow calling imported function
+			"    sfunc(); \n" // Allow calling a shared global function
+			"  } \n"
+			"  T @dup() const \n" // It must be possible for the shared class to use its own type
+			"  { \n"
+			"    T d; \n" // Calling the global factory as a shared function
+			"    return d; \n" 
+			"  } \n"
+			"  T() {} \n"
+			"  T(int a) \n"
+			"  { \n"
+			"     var = a; \n" // Constructor of shared class must not access non-shared code
+			"  } \n"
+			"  void f(badIntf @) {} \n" // Don't allow use of non-shared types in parameters/return type
+			"  ESHARED _es; \n" // allow
+			"  ESHARED2 _es2; \n" // allow
+			"  ENOTSHARED _ens; \n" // Don't allow
+			"  void f() \n"
+			"  { \n"
+			"    array<int> a; \n"
+			"  } \n"
+			"} \n"
+			"int var; \n"
+			"void gfunc() {} \n"
+			"shared void sfunc() \n"
+		    "{ \n"
+			"  gfunc(); \n" // don't allow
+			"  T t; \n" // allow
+			"  ESHARED2 s; \n" // allow
+	        "} \n"
+			"enum ENOTSHARED { ENS1 = 1 } \n"
+			"shared enum ESHARED2 { ES21 = 0 } \n"
+			"const int g_cnst = 42; \n"
+			"class nonShared {} \n"
+			"import void impfunc() from 'mod'; \n"
+			);
+		bout.buffer = "";
+		r = mod->Build();
+		if( r >= 0 ) 
+			TEST_FAILED;
+		if( bout.buffer != "a (32, 3) : Error   : Shared code cannot use non-shared type 'badIntf'\n"
+						   "a (3, 25) : Error   : Shared class cannot implement non-shared interface 'badIntf'\n"
+						   "a (35, 3) : Error   : Shared code cannot use non-shared type 'ENOTSHARED'\n"
+						   "a (5, 3) : Info    : Compiling void T::test()\n"
+						   "a (7, 5) : Error   : Shared code cannot access non-shared global variable 'var'\n"
+						   "a (8, 5) : Error   : Shared code cannot call non-shared function 'void gfunc()'\n"
+						   "a (11, 5) : Error   : Shared code cannot use non-shared type 'badIntf'\n"
+						   "a (14, 5) : Error   : Shared code cannot use non-shared type 'ENOTSHARED'\n"
+						   "a (15, 5) : Error   : Shared code cannot use non-shared type 'badIntf'\n"
+						   "a (17, 5) : Error   : Shared code cannot call non-shared function 'void gfunc()'\n"
+						   "a (18, 5) : Error   : Shared code cannot use non-shared type 'nonShared'\n"
+						   "a (18, 5) : Error   : Shared code cannot call non-shared function 'nonShared@ nonShared()'\n"
+						   "a (19, 5) : Error   : Shared code cannot call non-shared function 'void impfunc()'\n"
+						   "a (28, 3) : Info    : Compiling T::T(int)\n"
+		                   "a (30, 6) : Error   : Shared code cannot access non-shared global variable 'var'\n"
+						   "a (43, 1) : Info    : Compiling void sfunc()\n"
+		                   "a (45, 3) : Error   : Shared code cannot call non-shared function 'void gfunc()'\n" )
+		{
+			printf("%s", bout.buffer.c_str());
+			TEST_FAILED;
+		}
+		engine->DiscardModule("");
+
+		const char *validCode =
+			"shared interface I {} \n"
+			"shared class T : I \n"
+			"{ \n"
+			"  void func() {} \n"
+			"  int i; \n"
+			"} \n"
+			"shared void func() {} \n"
+			"shared enum eshare { e1, e2 } \n";
+		mod = engine->GetModule("1", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("a", validCode);
+		bout.buffer = "";
+		r = mod->Build();
+		if( r < 0 )
+			TEST_FAILED;
+
+		int t1 = mod->GetTypeIdByDecl("T");
+		if( t1 < 0 )
+			TEST_FAILED;
+
+		int f1 = mod->GetFunctionIdByDecl("void func()");
+		if( f1 < 0 )
+			TEST_FAILED;
+
+		int fact1 = engine->GetObjectTypeById(t1)->GetFactoryIdByIndex(0);
+		if( fact1 < 0 )
+			TEST_FAILED;
+
+		asIScriptModule *mod2 = engine->GetModule("2", asGM_ALWAYS_CREATE);
+		mod2->AddScriptSection("b", validCode);
+		r = mod2->Build();
+		if( r < 0 )
+			TEST_FAILED;
+
+		if( bout.buffer != "" )
+		{
+			printf("%s", bout.buffer.c_str());
+			TEST_FAILED;
+		}
+
+		int t2 = mod2->GetTypeIdByDecl("T");
+		if( t1 != t2 )
+			TEST_FAILED;
+
+		int f2 = mod2->GetFunctionIdByDecl("void func()");
+		if( f1 != f2 )
+			TEST_FAILED;
+
+		int fact2 = engine->GetObjectTypeById(t2)->GetFactoryIdByIndex(0);
+		if( fact1 != fact2 )
+			TEST_FAILED;
+
+		CBytecodeStream stream(__FILE__"1");
+		mod->SaveByteCode(&stream);
+
+		bout.buffer = "";
+		asIScriptModule *mod3 = engine->GetModule("3", asGM_ALWAYS_CREATE);
+		r = mod3->LoadByteCode(&stream);
+		if( r < 0 )
+			TEST_FAILED;
+
+		int t3 = mod3->GetTypeIdByDecl("T");
+		if( t1 != t3 )
+			TEST_FAILED;
+		if( bout.buffer != "" )
+		{
+			printf("%s", bout.buffer.c_str());
+			TEST_FAILED;
+		}
+
+		int f3 = mod3->GetFunctionIdByDecl("void func()");
+		if( f1 != f3 )
+			TEST_FAILED;
+
+		int fact3 = engine->GetObjectTypeById(t3)->GetFactoryIdByIndex(0);
+		if( fact1 != fact3 )
+			TEST_FAILED;
+
+		bout.buffer = "";
+		r = ExecuteString(engine, "T t; t.func(); func();", mod3);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+		if( bout.buffer != "" )
+		{
+			printf("%s", bout.buffer.c_str());
+			TEST_FAILED;
+		}
+
+		engine->Release();
+	}
+
+	// Test shared classes as members of other classes
+	// http://www.gamedev.net/topic/617717-shared-template-factory-stub/
+	{
+ 		engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+		engine->SetMessageCallback(asMETHOD(CBufferedOutStream,Callback), &bout, asCALL_THISCALL);
+
+		const char *code = 
+			"shared class S { int a; } \n"
+			"class A { S s; } \n";
+
+		asIScriptModule *mod = engine->GetModule("1", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("code", code);
+		bout.buffer = "";
+		r = mod->Build();
+		if( r < 0 )
+			TEST_FAILED;
+
+		mod = engine->GetModule("2", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("code", code);
+		bout.buffer = "";
+		r = mod->Build();
+		if( r < 0 )
+			TEST_FAILED;
+		if( bout.buffer != "" )
+		{
+			printf("%s", bout.buffer.c_str());
+			TEST_FAILED;
+		}
+
+		engine->Release();
+	}
+
+	// Test reloading scripts with shared code
+	// http://www.gamedev.net/topic/618417-problem-with-shared-keyword/
+	{
+ 		engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+		engine->SetMessageCallback(asMETHOD(CBufferedOutStream,Callback), &bout, asCALL_THISCALL);
+
+		const char *code = 
+			"shared class B {} \n"
+			"shared class iMyInterface { \n"
+			"  void MyFunc(const B &in) {} \n"
+			"} \n"
+			"class cMyClass : iMyInterface { \n"
+			"  void MyFunc(const B &in) { \n"
+			"  } \n"
+			"} \n";
+
+		asIScriptModule *mod = engine->GetModule("1", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("code", code);
+		bout.buffer = "";
+		r = mod->Build();
+		if( r < 0 )
+			TEST_FAILED;
+
+		int id = mod->GetTypeIdByDecl("cMyClass");
+		asIObjectType *type = engine->GetObjectTypeById(id);
+		asIScriptFunction *func = type->GetMethodByDecl("void MyFunc(const B &in)");
+		if( func == 0 )
+			TEST_FAILED;
+
+		asIScriptObject *obj = (asIScriptObject*)engine->CreateScriptObject(id);
+		asIScriptContext *ctx = engine->CreateContext();
+
+		ctx->Prepare(func);
+		ctx->SetObject(obj);
+		r = ctx->Execute();
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		ctx->Release();
+		obj->Release();
+
+		// This will orphan the shared types, but won't destroy them yet
+		// as they are still kept alive by the garbage collector
+		engine->DiscardModule("1");
+
+		// Build the module again. This will re-use the orphaned 
+		// shared classes, the code should still work normally
+		mod = engine->GetModule("1", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("code", code);
+		bout.buffer = "";
+		r = mod->Build();
+		if( r < 0 )
+			TEST_FAILED;
+
+		id = mod->GetTypeIdByDecl("cMyClass");
+		type = engine->GetObjectTypeById(id);
+		func = type->GetMethodByDecl("void MyFunc(const B &in)");
+		if( func == 0 )
+			TEST_FAILED;
+
+		obj = (asIScriptObject*)engine->CreateScriptObject(id);
+		ctx = engine->CreateContext();
+
+		ctx->Prepare(func);
+		ctx->SetObject(obj);
+		r = ctx->Execute();
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		ctx->Release();
+		obj->Release();
+
+		engine->Release();
+	}
+
+	// Test problem reported by TheAtom
+	// http://www.gamedev.net/topic/622841-shared-issues/
+	{
+ 		engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+		engine->SetMessageCallback(asMETHOD(CBufferedOutStream,Callback), &bout, asCALL_THISCALL);
+
+		const char *code = 
+			"shared int f() \n"
+			"{ \n"
+			"  return 0; \n"
+			"} \n";
+
+		asIScriptModule *mod = engine->GetModule("1", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection("code", code);
+		bout.buffer = "";
+		r = mod->Build();
+		if( r < 0 )
+			TEST_FAILED;
+
+		if( bout.buffer != "" )
+		{
+			printf("%s", bout.buffer.c_str());
+			TEST_FAILED;
+		}		
+
+		engine->Release();
+	}
+
+	// Success
+	return fail;
+}
+
+
+
+} // namespace
+
