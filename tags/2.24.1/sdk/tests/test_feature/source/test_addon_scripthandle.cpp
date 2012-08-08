4c8ca83fbbcb4946f6f4e1954cc50198a3c4a358
angelcode <angelcode@404ce1b2-830e-0410-a2e2-b09542c77caf>
2012-08-08 00:47:33 +0000

    tagging 2.24.1
    git-svn-id: http://svn.code.sf.net/p/angelscript/code@1387 404ce1b2-830e-0410-a2e2-b09542c77caf

---
+++ tags/2.24.1/sdk/tests/test_feature/source/test_addon_scripthandle.cpp
@@ -0,0 +1,185 @@
+#include "utils.h"
+#include "../../../add_on/scripthandle/scripthandle.h"
+#include "../../../add_on/scriptarray/scriptarray.h"
+
+namespace Test_Addon_ScriptHandle
+{
+
+static const char *TESTNAME = "Test_Addon_ScriptHandle";
+
+static void ReceiveRefByValue(CScriptHandle /*hndl*/)
+{
+}
+
+static void ReceiveRefByRef(CScriptHandle &/*hndl*/)
+{
+}
+
+static CScriptHandle ReturnRef()
+{
+	asIScriptContext *ctx = asGetActiveContext();
+	asIScriptEngine *engine = ctx->GetEngine();
+	asIScriptModule *mod = engine->GetModule("test");
+	asIObjectType *type = mod->GetObjectTypeByName("CTest");
+
+	asIScriptObject *obj = reinterpret_cast<asIScriptObject *>(engine->CreateScriptObject(type->GetTypeId()));
+
+	CScriptHandle ref;
+	ref.Set(obj, type);
+
+	// Need to release our reference as the CScriptHandle counts its own, and we will not keep our reference
+	obj->Release();
+
+	return ref;
+}
+
+bool Test()
+{
+	bool fail = false;
+	int r;
+	COutStream out;
+	CBufferedOutStream bout;
+	asIScriptContext *ctx;
+ 	asIScriptEngine *engine;
+
+	{
+		engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+		engine->SetMessageCallback(asMETHOD(COutStream,Callback), &out, asCALL_THISCALL);
+		RegisterScriptHandle(engine);
+		RegisterScriptArray(engine, false);
+		engine->RegisterGlobalFunction("void assert(bool)", asFUNCTION(Assert), asCALL_GENERIC);
+		engine->RegisterGlobalFunction("void ReceiveRefByVal(ref@)", asFUNCTION(ReceiveRefByValue), asCALL_CDECL);
+		engine->RegisterGlobalFunction("void ReceiveRefByRef(ref&in)", asFUNCTION(ReceiveRefByRef), asCALL_CDECL);
+		engine->RegisterGlobalFunction("ref @ReturnRef()", asFUNCTION(ReturnRef), asCALL_CDECL);
+
+		// TODO: optimize: assert( ha !is null ); is producing code that unecessarily calls ClrVPtr and FREE for the null handle
+		const char *script = 
+							 "class A {} \n"
+							 "class B {} \n"
+							 "void main() \n"
+							 "{ \n"
+							 "  ref@ ra, rb; \n"
+							 "  A a; B b; \n"
+							 // Assignment of reference
+							 "  @ra = @a; \n"
+							 "  assert( ra is a ); \n" 
+							 "  @rb = @b; \n"
+							 // Casting to reference
+							 "  A@ ha = cast<A>(ra); \n"
+							 "  assert( ha !is null ); \n"
+							 "  B@ hb = cast<B>(ra); \n"
+							 "  assert( hb is null ); \n"
+							 // Assigning null, and comparing with null
+							 "  @ra = null; \n"
+							 "  assert( ra is null ); \n"
+							 "  func2(ra); \n"
+							 // Handle assignment with explicit handle
+							 "  @ra = @rb; \n"
+							 "  assert( ra is b ); \n"
+							 "  assert( rb is b ); \n"
+							 "  assert( ra is rb ); \n"
+							 // Handle assignment with implicit handle
+							 "  @rb = rb; \n"
+							 "  assert( rb is b ); \n"
+							 "  assert( ra is rb ); \n"
+							 // Function call and return value
+							 "  @rb = func(rb); \n"
+							 "  assert( rb is b ); \n"
+							 "  assert( func(rb) is b ); \n"
+							 // Arrays of handles 
+							 "  array<ref@> arr(2); \n"
+							 "  assert( arr[0] is null ); \n"
+							 "  @arr[0] = a; \n"
+							 "  @arr[1] = a; \n"
+							 "  assert( arr[0] is arr[1] ); \n"
+							 "  assert( arr[0] is a ); \n"
+							 // Implicit conv from type to ref
+							 "  func2(null); \n"
+							 "  func(a); \n"
+							 "  assert( func(a) is a ); \n"
+							 "} \n"
+							 "ref@ func(ref@ r) { return r; } \n"
+							 "void func2(ref@r) { assert( r is null ); } \n"
+							 "interface ITest {} \n"
+							 "class CBase {} \n"
+							 "class CTest : ITest, CBase \n"
+							 "{ \n"
+							 "  int val; \n"
+							 "  CTest() \n"
+							 "  { \n"
+							 "    val = 42; \n"
+							 "  } \n"
+							 "} \n";
+
+		asIScriptModule *mod = engine->GetModule("test", asGM_ALWAYS_CREATE);
+		mod->AddScriptSection(TESTNAME, script);
+		r = mod->Build();
+		if( r < 0 )
+		{
+			TEST_FAILED;
+			printf("%s: Failed to compile the script\n", TESTNAME);
+		}
+
+		ctx = engine->CreateContext();
+		r = ExecuteString(engine, "main()", mod, ctx);
+		if( r != asEXECUTION_FINISHED )
+		{
+			if( r == asEXECUTION_EXCEPTION )
+				PrintException(ctx, true);
+
+			printf("%s: Failed to execute script\n", TESTNAME);
+			TEST_FAILED;
+		}
+		if( ctx ) ctx->Release();
+
+		r = ExecuteString(engine, "ref @r; ReceiveRefByVal(r);", mod);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		// This will cause an implicit cast to 'ref'. The object must be release properly afterwards
+		r = ExecuteString(engine, "ReceiveRefByRef(A());", mod);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		// Test return by ref
+		r = ExecuteString(engine, "ref @r = ReturnRef(); \n"
+								  "assert( r !is null ); \n"
+		                          "CTest @t = cast<CTest>(r); \n"
+								  "assert( t !is null ); \n"
+								  "assert( t.val == 42 ); \n", mod);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		// Test that the cast op can determine relationship between object types
+		r = ExecuteString(engine, "ref @r(CTest()); \n"
+								  "ITest @t = cast<ITest>(r); \n"
+								  "assert( t !is null ); \n"
+								  "CBase @b = cast<CBase>(r); \n"
+								  "assert( b !is null ); \n"
+								  "@r = null; \n" // Clear the content
+								  "@r = b; \n" // Set the same object, but this time as the base type
+								  "@t = cast<ITest>(r); \n"
+								  "assert( t !is null ); \n"
+								  "CTest @d = cast<CTest>(r); \n"
+								  "assert( d !is null ); \n", mod);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		// Test that it is possible to cast the return value directly
+		r = ExecuteString(engine, "CTest @t = cast<CTest>(ReturnRef()); \n"
+                                  "assert( t !is null ); \n"
+                                  "assert( t.val == 42 ); \n", mod);
+		if( r != asEXECUTION_FINISHED )
+			TEST_FAILED;
+
+		engine->Release();
+	}
+
+
+	// Success
+	return fail;
+}
+
+
+} // namespace
+
---
+++ tags/2.24.1/sdk/tests/test_performance/source/main.cpp
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#if defined(WIN32)
+#include <conio.h>
+#endif
+#if defined(_MSC_VER)
+#include <crtdbg.h>
+#endif
+#include "angelscript.h"
+
+namespace TestBasic        { void Test(double *time); }
+namespace TestBasic2       { void Test(double *time); }
+namespace TestCall         { void Test(double *time); }
+namespace TestCall2        { void Test(double *time); }
+namespace TestFib          { void Test(double *time); }
+namespace TestInt          { void Test(double *time); }
+namespace TestIntf         { void Test(double *time); }
+namespace TestMthd         { void Test(double *time); }
+namespace TestString       { void Test(double *time); }
+namespace TestStringPooled { void Test(double *time); }
+namespace TestString2      { void Test(double *time); }
+namespace TestThisProp     { void Test(double *time); }
+namespace TestVector3      { void Test(double *time); }
+namespace TestAssign       { void Test(double *times); }
+
+// Times for 2.23.0
+double testTimesOrig[18] = 
+{1.25, // Basic
+.277,  // Basic2
+1.17,  // Call
+1.55,  // Call2
+1.70,  // Fib
+.280,  // Int
+1.78,  // Intf
+1.56,  // Mthd
+1.83,  // String
+.910,  // String2
+1.14,  // StringPooled
+.714,  // ThisProp
+.382,  // Vector3
+.459,  // Assign.1
+.663,  // Assign.2
+.406,  // Assign.3
+.551,  // Assign.4
+.560   // Assign.5
+};
+
+// Times for 2.23.1 WIP
+double testTimesOrig2[18] = 
+{
+1.17,  // Basic
+.359,  // Basic2
+1.21,  // Call
+1.63,  // Call2
+1.87,  // Fib
+.234,  // Int
+1.84,  // Intf
+1.63,  // Mthd
+1.87,  // String
+.858,  // String2
+1.15,  // StringPooled
+.702,  // ThisProp
+.374,  // Vector3
+.421,  // Assign.1
+.749,  // Assign.2
+.390,  // Assign.3
+.530,  // Assign.4
+.515   // Assign.5
+};
+
+double testTimesBest[18];
+double testTimes[18];
+
+void DetectMemoryLeaks()
+{
+#if defined(_MSC_VER)
+	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF|_CRTDBG_ALLOC_MEM_DF);
+	_CrtSetReportMode(_CRT_ASSERT,_CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT,_CRTDBG_FILE_STDERR);
+#endif
+}
+
+int main(int argc, char **argv)
+{
+	DetectMemoryLeaks();
+
+	printf("Performance test");
+#ifdef _DEBUG 
+	printf(" (DEBUG)");
+#endif
+	printf("\n");
+	printf("AngelScript %s\n", asGetLibraryVersion()); 
+
+	for( int n = 0; n < 18; n++ )
+		testTimesBest[n] = 1000;
+
+	for( int n = 0; n < 3; n++ )
+	{
+		TestBasic::Test(&testTimes[0]); printf("."); fflush(stdout);
+		TestBasic2::Test(&testTimes[1]); printf("."); fflush(stdout);
+		TestCall::Test(&testTimes[2]); printf("."); fflush(stdout);
+		TestCall2::Test(&testTimes[3]); printf("."); fflush(stdout);
+		TestFib::Test(&testTimes[4]); printf("."); fflush(stdout);
+		TestInt::Test(&testTimes[5]); printf("."); fflush(stdout);
+		TestIntf::Test(&testTimes[6]); printf("."); fflush(stdout);
+		TestMthd::Test(&testTimes[7]); printf("."); fflush(stdout);
+		TestString::Test(&testTimes[8]); printf("."); fflush(stdout);
+		TestString2::Test(&testTimes[9]); printf("."); fflush(stdout);
+		TestStringPooled::Test(&testTimes[10]); printf("."); fflush(stdout);
+		TestThisProp::Test(&testTimes[11]); printf("."); fflush(stdout);
+		TestVector3::Test(&testTimes[12]); printf("."); fflush(stdout);
+		TestAssign::Test(&testTimes[13]); printf("."); fflush(stdout);
+
+		for( int t = 0; t < 18; t++ )
+		{
+			if( testTimesBest[t] > testTimes[t] )
+				testTimesBest[t] = testTimes[t];
+		}
+
+		printf("\n");
+	}
+	
+	printf("Basic          %.3f    %.3f    %.3f%s\n", testTimesOrig[ 0], testTimesOrig2[ 0], testTimesBest[ 0], testTimesBest[ 0] < testTimesOrig2[ 0] ? " +" : " -"); 
+	printf("Basic2         %.3f    %.3f    %.3f%s\n", testTimesOrig[ 1], testTimesOrig2[ 1], testTimesBest[ 1], testTimesBest[ 1] < testTimesOrig2[ 1] ? " +" : " -"); 
+	printf("Call           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 2], testTimesOrig2[ 2], testTimesBest[ 2], testTimesBest[ 2] < testTimesOrig2[ 2] ? " +" : " -"); 
+	printf("Call2          %.3f    %.3f    %.3f%s\n", testTimesOrig[ 3], testTimesOrig2[ 3], testTimesBest[ 3], testTimesBest[ 3] < testTimesOrig2[ 3] ? " +" : " -"); 
+	printf("Fib            %.3f    %.3f    %.3f%s\n", testTimesOrig[ 4], testTimesOrig2[ 4], testTimesBest[ 4], testTimesBest[ 4] < testTimesOrig2[ 4] ? " +" : " -"); 
+	printf("Int            %.3f    %.3f    %.3f%s\n", testTimesOrig[ 5], testTimesOrig2[ 5], testTimesBest[ 5], testTimesBest[ 5] < testTimesOrig2[ 5] ? " +" : " -"); 
+	printf("Intf           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 6], testTimesOrig2[ 6], testTimesBest[ 6], testTimesBest[ 6] < testTimesOrig2[ 6] ? " +" : " -"); 
+	printf("Mthd           %.3f    %.3f    %.3f%s\n", testTimesOrig[ 7], testTimesOrig2[ 7], testTimesBest[ 7], testTimesBest[ 7] < testTimesOrig2[ 7] ? " +" : " -"); 
+	printf("String         %.3f    %.3f    %.3f%s\n", testTimesOrig[ 8], testTimesOrig2[ 8], testTimesBest[ 8], testTimesBest[ 8] < testTimesOrig2[ 8] ? " +" : " -"); 
+	printf("String2        %.3f    %.3f    %.3f%s\n", testTimesOrig[ 9], testTimesOrig2[ 9], testTimesBest[ 9], testTimesBest[ 9] < testTimesOrig2[ 9] ? " +" : " -"); 
+	printf("StringPooled   %.3f    %.3f    %.3f%s\n", testTimesOrig[10], testTimesOrig2[10], testTimesBest[10], testTimesBest[10] < testTimesOrig2[10] ? " +" : " -"); 
+	printf("ThisProp       %.3f    %.3f    %.3f%s\n", testTimesOrig[11], testTimesOrig2[11], testTimesBest[11], testTimesBest[11] < testTimesOrig2[11] ? " +" : " -"); 
+	printf("Vector3        %.3f    %.3f    %.3f%s\n", testTimesOrig[12], testTimesOrig2[12], testTimesBest[12], testTimesBest[12] < testTimesOrig2[12] ? " +" : " -"); 
+	printf("Assign.1       %.3f    %.3f    %.3f%s\n", testTimesOrig[13], testTimesOrig2[13], testTimesBest[13], testTimesBest[13] < testTimesOrig2[13] ? " +" : " -"); 
+	printf("Assign.2       %.3f    %.3f    %.3f%s\n", testTimesOrig[14], testTimesOrig2[14], testTimesBest[14], testTimesBest[14] < testTimesOrig2[14] ? " +" : " -"); 
+	printf("Assign.3       %.3f    %.3f    %.3f%s\n", testTimesOrig[15], testTimesOrig2[15], testTimesBest[15], testTimesBest[15] < testTimesOrig2[15] ? " +" : " -"); 
+	printf("Assign.4       %.3f    %.3f    %.3f%s\n", testTimesOrig[16], testTimesOrig2[16], testTimesBest[16], testTimesBest[16] < testTimesOrig2[16] ? " +" : " -"); 
+	printf("Assign.5       %.3f    %.3f    %.3f%s\n", testTimesOrig[17], testTimesOrig2[17], testTimesBest[17], testTimesBest[17] < testTimesOrig2[17] ? " +" : " -"); 
+
+	printf("--------------------------------------------\n");
+	printf("Press any key to quit.\n");
+#if defined(WIN32)
+	while(!_getch());
+#endif
+	return 0;
+}
